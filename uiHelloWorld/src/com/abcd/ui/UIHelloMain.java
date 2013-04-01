d81f288eeef54afc76fa8743444c551d9e0214b8
Nits P <sousanit@yahoo.com>
2013-03-31 21:37:37 -0400

    this is 1st commit.

---
+++ uiHelloWorld/src/com/abcd/ui/UIHelloMain.java
@@ -0,0 +1,14 @@
+package com.abcd.ui;
+
+public class UIHelloMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a 1st line");
+
+	}
+
+}
